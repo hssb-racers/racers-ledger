2735fbb89364c105230b13782caa0875aaef771a
Sariya Melody <sariya@sariya.garden>
2021-04-14 01:36:44 -0400

    implement extremely extremely basic version of RACErs Companion -- has a single button. click it to find a CSV file to load and visualize your ledger. this is some nice progress on #2.
    Lots and lots and lots of areas to improve:
     - compare multiple runs?
     - live stream?
     - figure out how the fuck to release this companion app????
     - significantly improve graphing performance
    
    but hey, it works pretty well, given that I didn't know WPF at all a few hours ago!

---
+++ RACErsCompanion/SalvageGraph.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using RACErsLedger;
+
+namespace RACErsCompanion
+{
+    /// <summary>
+    /// Interaction logic for SalvageGraph.xaml
+    /// </summary>
+    public partial class SalvageGraph : UserControl
+    {
+        public SalvageGraph()
+        {
+            InitializeComponent();
+
+            var seriesConfig = Mappers.Xy<SalvageEntryData>().X(e => e.GameTime).Y(e => e.CumulativeValue);
+
+            SeriesCollection = new SeriesCollection(seriesConfig)
+            {
+
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+    }
+}
