d31416b535493bf6f4663f17280009b386927b4d
Sariya Melody <sariya@sariya.garden>
2021-05-06 00:17:42 -0400

    reformat main.rs to be sytlistically consistent

--- RACErsLedgerLamprey/src/main.rs
+++ RACErsLedgerLamprey/src/main.rs
@@ -1,13 +1,13 @@
 mod datatypes;
 pub use crate::datatypes::*;
 
+use log::{debug, error, info, LevelFilter};
+use serde_json::{Result, Value};
+use simple_logger::SimpleLogger;
 use std::net::TcpListener;
 use std::thread::spawn;
-use url::Url;
 use tungstenite::{connect, server::accept, Message};
-use log::{LevelFilter, debug, error, info};
-use simple_logger::SimpleLogger;
-use serde_json::{Result, Value};
+use url::Url;
 
 use clap::{AppSettings, Clap};
 
@@ -25,19 +25,25 @@ struct Opts {
 }
 
 fn main() {
-    let opts: Opts = Opts::parse();
-    let level = match opts.verbose {
-        0 => LevelFilter::Error,
-        1 => LevelFilter::Info,
-        2 => LevelFilter::Debug,
-        3 | _ => LevelFilter::Trace
-    };
-    SimpleLogger::new().with_level(level).init().unwrap();
+    let opts = Opts::parse();
+    SimpleLogger::new()
+        .with_level(match opts.verbose {
+            0 => LevelFilter::Error,
+            1 => LevelFilter::Info,
+            2 => LevelFilter::Debug,
+            3 | _ => LevelFilter::Trace,
+        })
+        .init()
+        .unwrap();
     info!("starting up server");
-    info!("connect port: {}, listen port: {}", opts.connect_port, opts.listen_port);
+    info!(
+        "connect port: {}, listen port: {}",
+        opts.connect_port, opts.listen_port
+    );
 
     let connect_destination = format!("ws://localhost:{}/racers-ledger/", opts.connect_port);
-    let (mut socket, response) = connect(Url::parse(connect_destination.as_str()).unwrap()).expect(format!("Can't connect to {}", connect_destination).as_str());
+    let (mut socket, response) = connect(Url::parse(connect_destination.as_str()).unwrap())
+        .expect(format!("Can't connect to {}", connect_destination).as_str());
     info!("connected to server");
     info!("response code: {}", response.status());
     loop {
@@ -50,21 +56,21 @@ fn main() {
                 match event {
                     Ok(salvage_event) => {
                         info!("decoded {:#?}", salvage_event)
-                    },
+                    }
                     Err(e) => {
                         error!("{}", e)
                     }
                 }
-            },
+            }
             Message::Ping(data) => {
                 debug!("received ping! (data: {:?})", data);
-            },
+            }
             Message::Pong(data) => {
                 debug!("received pong! (data: {:?}", data);
-            },
+            }
             Message::Binary(data) => {
                 debug!("received binary data: {:?}", data)
-            },
+            }
             Message::Close(close_frame) => {
                 debug!("received close!");
                 if let Some(close_frame) = close_frame {
